aecd89591599aeac07a7ebc672cfb7ec34d93e80
Juan Martinez <JuanMartinezManzanilla@hotmail.com>
2024-05-24 20:13:27 -0600

    cambios a predicciones.py

--- pages/predicciones.py
+++ pages/predicciones.py
@@ -20,21 +20,25 @@ realtimepred = face_rec.RealTimePred() # real time prediction class
 
 def video_frame_callback(frame):
     global setTime
-    img = frame.to_ndarray(format="bgr24")
+
+    img = frame.to_ndarray(format="bgr24") # 3 dimension numpy array
+    # operation that you can perform on the array
     pred_img = realtimepred.face_prediction(img,redis_face_db,
-                                        'face',['name','Role'],thresh=0.5)
-    
+                                        'facial_features',['name','Role'],thresh=0.5)
+
     timenow = time.time()
     difftime = timenow - setTime
     if difftime >= waitTime:
         realtimepred.saveLogs_redis()
-        setTime = time.time() # reset time        
+        setTime = time.time() # reset time
         print('Save Data to redis database')
 
-    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+    return av.VideoFrame.from_ndarray(pred_img, format="bgr24")
 
 
-webrtc_streamer(key="realtime", video_frame_callback=video_frame_callback,
-                 rtc_configuration={
+webrtc_streamer(key="realtimePrediction", video_frame_callback=video_frame_callback,
+rtc_configuration={
         "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
-    })
+    }
+)
